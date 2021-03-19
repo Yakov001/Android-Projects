a2f0b84f1d1a91de7f70df7fe3fa1d049bfe7c65
Yakov <yakovrussian@gmail.com>
2021-03-19 11:27:15 +0300

    Before working with image for each activity.

--- NerdLauncher/app/src/main/java/android/bignerdranch/nerdlauncher/NerdLauncherFragment.kt
+++ NerdLauncher/app/src/main/java/android/bignerdranch/nerdlauncher/NerdLauncherFragment.kt
@@ -1,12 +1,16 @@
 package android.bignerdranch.nerdlauncher
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -48,12 +52,59 @@ class NerdLauncherFragment : Fragment(){
 
         Collections.sort(activities) { a: ResolveInfo, b: ResolveInfo ->
             var pm = activity?.packageManager
-            return@sort String.CASE_INSENSITIVE_ORDER.compare(
+            String.CASE_INSENSITIVE_ORDER.compare(
                 a.loadLabel(pm).toString(),
                 b.loadLabel(pm).toString()
             )
         }
 
         Log.i(TAG, "Found ${activities.size} activities")
+        mRecyclerView.adapter = ActivityAdapter(activities)
+    }
+
+    //---------------------------------------------------------------------------//
+
+    private class ActivityHolder(itemView : View) : RecyclerView.ViewHolder(itemView) , View.OnClickListener {
+        private lateinit var mResolveInfo : ResolveInfo
+        private var mNameTextView : TextView = itemView as TextView
+
+        fun bindActivity (resolveInfo: ResolveInfo) {
+            mResolveInfo = resolveInfo
+            var ctxt : Context = itemView.context
+            var pm : PackageManager = ctxt.packageManager
+            mNameTextView.text = mResolveInfo.loadLabel(pm).toString()
+            mNameTextView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            var activityInfo = mResolveInfo.activityInfo
+            var i : Intent = Intent(Intent.ACTION_MAIN).setClassName(activityInfo.applicationInfo.packageName,
+            activityInfo.name).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(v!!.context, i, null)
+        }
+    }
+
+    //---------------------------------------------------------------------------//
+
+    private class ActivityAdapter(activities : List<ResolveInfo>) :
+        RecyclerView.Adapter<ActivityHolder>() {
+
+        private val mActivities = activities
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivityHolder {
+            var layoutInflater = LayoutInflater.from(parent.context)
+            var view = layoutInflater.inflate(android.R.layout.simple_list_item_2, parent, false)
+            return ActivityHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ActivityHolder, position: Int) {
+            var mResolveInfo = mActivities.get(position)
+            holder.bindActivity(mResolveInfo)
+        }
+
+        override fun getItemCount(): Int {
+            return mActivities.size
+        }
+
     }
 }
